0b9ab315224711738182148af10e3e98fdf089db
MikeT <mikejamesthompson@gmail.com>
2014-02-26 22:23:28 +0000

    Updated requirements.txt

--- run.py
+++ run.py
@@ -1,12 +1,3 @@
 from app import app
 
-app.debug = True
-
-if app.debug:
-	import logging
-	from logging import FileHandler
-	file_handler = FileHandler('app.log')
-	file_handler.setLevel(logging.WARNING)
-	app.logger.addHandler(file_handler)
-
-app.run(debug = app.debug)
+app.run(debug = True)
