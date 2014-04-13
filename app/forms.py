db790e04b89a8b8fb52c2ec58f3cdbf30fdf6720
MikeT <mikejamesthompson@gmail.com>
2014-04-13 20:58:13 +0100

    small styling changes

--- app/forms.py
+++ app/forms.py
@@ -2,4 +2,4 @@ from flask.ext.wtf import Form
 from wtforms import fields, validators
 
 class SearchForm(Form):
-    road = fields.StringField(u'road', validators = [validators.InputRequired(), validators.Length(min=3,message="Please enter a road name")])
+    road = fields.StringField(u'road', validators = [validators.InputRequired(), validators.Length(min=1,message="Please enter a road name")])
