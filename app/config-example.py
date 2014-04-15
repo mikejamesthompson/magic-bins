b864a6d82434b3845f815a7efe580b283f5f7d94
MikeT <mikejamesthompson@gmail.com>
2014-04-15 08:56:02 +0100

    GA tracking

--- app/config-example.py
+++ app/config-example.py
@@ -7,3 +7,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 # WTF settings
 CSRF_ENABLED = True
 SECRET_KEY = 'boom-shake-shake-shake-the-room'
+
+# Google Analytics Tracking details
+GA_ACCOUNT_ID = 'UA-00000001-1'
+GA_ACCOUNT_DOMAIN = 'example.com'
