cd64aaff89850cb583bb5833a2bb45b9593b5fb6
root <root@marshall.artifactual.co.uk>
2014-05-18 16:56:50 +0000

    changes for production to work properly, needs to be generalised though

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
-activate_this = '/var/www/bins.artifactual.co.uk/venv/bin/activate_this.py'
+activate_this = '/var/www/sevenoaks.bintim.es/venv/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 import sys
-sys.path.insert(0, '/var/www/bins.artifactual.co.uk/source')
+sys.path.insert(0, '/var/www/sevenoaks.bintim.es/src')
 from app import app as application
 
 if __name__ == '__main__':
